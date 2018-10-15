1c9ffd267d8f486d3ca199316dc9a78b8b51c8c5
MatthieuJehanno <matthieujehanno@outlook.fr>
2018-10-16 00:34:52 +0200

    f

--- application/views/inscription.php
+++ application/views/inscription.php
@@ -10,26 +10,22 @@
 
             <h2><center>Inscrivez-vous</center> </h2> 
          
-           <div class="form-group"> 
                 <h5>Nom</h5>
                 <input type="text" class="form-control" name="nom"  value="Votre nom">
-            </div> 
-            <div class="form-group">
+            
                 <h5>Prénom</h5>
                 <input type="text" class="form-control" name="prenom"  value="Votre prénom">
-            </div>
-            <div class="form-group">
+            
                 <h5>Identifiant</h5>
                 <input type="login" class="form-control" name="login"  value="Votre identifiant">
-            </div>
-            <div class="form-group">
+            
                 <h5>Mot de passe</h5>
                 <input type="password" class="form-control" name="mdp" value="Votre mot de passe">
-            </div>
+            
 
            
         <button  id="submit" class="btn btn-primary">Inscription</button>
-    <div id="progress" style="display:none;text-align:center;"><img src="img/progress.gif"/></div>
+    
 
 
 </body>
