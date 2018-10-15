b2c84cd425e9cbd66e648c84af17c94172cfe533
MatthieuJehanno <matthieujehanno@outlook.fr>
2018-10-16 00:29:10 +0200

    continue

---
+++ application/models/Model_Inscription.php
@@ -0,0 +1,23 @@
+<?php
+
+class ModeleUtilisateur extends CI_Model{
+    private $idUser;
+    private $nomUser;
+    private $login;
+    private $mdp;
+
+    function __construct(){
+         parent::__construct();
+    }
+
+    function getById($id){
+         $limit=1;
+         $offset=0;
+         $requete = $this->db->get_where('users', array('id' => $id), $limit, $offset);
+         return $this->db->query($requete )->result_array();
+    }
+
+    //--- Ajouter insert et update
+}
+
+?>
