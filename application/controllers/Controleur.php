711a7307c97e268d3fa87641823c23891b1816c2
LermeRomain <lermeromain@hotmail.fr>
2018-10-16 00:26:46 +0200

    modif

--- application/controllers/Controleur.php
+++ application/controllers/Controleur.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Welcome extends CI_Controller {
+class Controleur extends CI_Controller {
 
 	/**
 	 * Index Page for this controller.
@@ -20,6 +20,7 @@ class Welcome extends CI_Controller {
 	 */
 	public function index()
 	{
-		$this->load->view('welcome_message');
+		$this->load->helper('url');
+		$this->load->view('header');
 	}
 }
