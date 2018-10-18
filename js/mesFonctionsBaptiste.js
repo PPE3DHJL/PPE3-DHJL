24999c650ea106888493da7ec6881d032026c08d
BHurel <bhurel@lerebours.org>
2018-10-18 21:16:29 +0200

    creation fonctions Baptiste

--- js/mesFonctionsBaptiste.js
+++ js/mesFonctionsBaptiste.js
@@ -1,17 +1,3 @@
-function connexion() {
-    $.ajax({
-        type: "POST",
-        data: "login=" + $("input[name=login]").val() + "&mdp=" + $("input[name=mdp]").val(),
-        success: function(data) {
-            $("input[name=login]").val("");
-            $("input[name=mdp]").val("");
-        },
-        error: function(data) {
-            alert("Erreur: connexion");
-        }
-    }
-    );
-}
 function CreationOffre()
 {
     $.ajax
