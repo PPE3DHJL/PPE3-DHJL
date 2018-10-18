3d6815c3ca68d8a289adf8ef2ffbd17839b98e7a
BHurel <bhurel@lerebours.org>
2018-10-18 22:52:45 +0200

    creation controleur + modif header + regroupement fonctions

--- application/views/header.php
+++ application/views/header.php
@@ -13,10 +13,10 @@
         <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="#">Mes Demandes</a>
+        <a class="nav-link" href="<?php echo site_url('ControleurNavigation/Demandes') ?>">Mes Demandes</a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="#">Mes Offres</a>
+        <a class="nav-link" href="<?php echo site_url('ControleurNavigation/Offres') ?>">Mes Offres</a>
       </li>
       <li class="nav-item">
         <a class="nav-link" href="#">Les Deals</a>
