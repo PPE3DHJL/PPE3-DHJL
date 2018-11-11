ee0d2fb45ff94625d204cab0812a0a7749564492
LermeRomain <lermeromain@hotmail.fr>
2018-11-11 16:02:40 +0100

    connections

---
+++ application/models/Model_User.php
@@ -0,0 +1,9 @@
+<?php
+class modelUser extends CI_model{
+    function getUser($idLogin){
+        $sql = $this->db->query("select idUser, nomUser, login, mdp, photoUser from user where idUser=$idLogin");
+        return $sql->result();
+        }
+    }
+
+?>
